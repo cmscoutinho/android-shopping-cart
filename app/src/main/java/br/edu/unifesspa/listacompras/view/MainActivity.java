03118afb9d6a5c8d79af00c326d1e27f8961818c
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-27 10:10:44 -0300

    disable loadSampleItems()

--- app/src/main/java/br/edu/unifesspa/listacompras/view/MainActivity.java
+++ app/src/main/java/br/edu/unifesspa/listacompras/view/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         initComponents();
         initListeners();
         updateList();
-        loadSampleItems();
+//        loadSampleItems();
     }
 
     @Override
