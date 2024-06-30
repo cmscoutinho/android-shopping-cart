26734520b9621daa0dd4c9c959becad1dcffcc3b
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-29 23:46:20 -0300

    finish singlechoice sorting

--- app/src/main/java/br/edu/unifesspa/listacompras/view/MainActivity.java
+++ app/src/main/java/br/edu/unifesspa/listacompras/view/MainActivity.java
@@ -90,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             } else if (id == R.id.fab_sort) {
                 dialogListSort();
-                updateList();
                 return false;
             } else if (id == R.id.fab_clear) {
                 productDAO.clearList();
@@ -106,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("position", position);
             startActivity(intent);
         });
-
         productListView.setOnItemLongClickListener((parent, view, position, id) -> {
             new AlertDialog.Builder(this)
                     .setTitle("Remoção de item")
@@ -148,30 +146,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void dialogListSort() {
-        productDAO.sortList(0); // to be removed after implementing the MultiChoice
 
-        String[] items = {"Teste",getString(R.string.name), getString(R.string.price), getString(R.string.quantity)};
-        boolean[] itemsValues = {false, false, false, false};
-        new AlertDialog.Builder(this)
-                .setTitle("Ordenar lista")
-                .setMessage("Como gostaria de ordenar a lista")
-                .setMultiChoiceItems(items, itemsValues, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+        String[] items = {getString(R.string.name), getString(R.string.price), getString(R.string.quantity)};
+        final int[] checkedItem = {-1};
 
-                    }
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Ordenar por:")
+                .setSingleChoiceItems(items, checkedItem[0], (dialog, which) -> {
+                    productDAO.sortList(which);
+                    updateList();
+                    dialog.dismiss();
                 })
-                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                .setNeutralButton("Cancela", (dialog, which) -> {
 
-                    }
-                })
-                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Handle Cancel button press
-                    }
                 })
                 .create()
                 .show();
